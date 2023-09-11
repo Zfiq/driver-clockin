e0c1694769bbb0343117c94b732006fdc1d9ed08
Zfiq <71498625+Zfiq@users.noreply.github.com>
2023-09-11 13:34:13 +0100

    Date input search Fixed

--- Controller/ActivityHistory.js
+++ Controller/ActivityHistory.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import Toast from 'react-native-toast-message';
 import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet, FlatList, Pressable} from 'react-native';
